8c801c06dcff9997f41c5fd0492a822e8adf85ea
Pablo Alejandro <pablo@pabli.net.ar>
2025-10-09 20:50:42 -0300

    mejoras con variable de entorno APT_SEARCH

--- apt_trixie.py
+++ apt_trixie.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from pathlib import Path
-from os import access, execv, geteuid, X_OK
+from os import access, execv, environ, geteuid, X_OK
 from os.path import exists
 from sys import argv, exit
 
 argv[0] = '/usr/bin/' + Path(argv[0]).stem[:3]
+apt_search = environ.get("APT_SEARCH", argv[0])
 
 WITH_SUDO = [
     'install','remove', 'purge', 'update', 'upgrade', 'full-upgrade',
@@ -17,10 +18,8 @@ cmds = ['/usr/bin/aptitude', '/usr/bin/apt-cache', argv[0]]
 if len(argv) > 1:
     for e in argv[1:]:
         if e == 'search':
-            for cmd in cmds:
-                if exists(cmd) and access(cmd, X_OK):
-                    argv[0] = cmd
-                    break
+            if not access(apt_search, X_OK):
+                argv[0] = apt_search
             break
         elif geteuid() > 0 and e in WITH_SUDO:
             argv.insert(0, '/usr/bin/sudo')
--- apt_trixie.sh
+++ apt_trixie.sh
@@ -1,48 +1,46 @@
 #!/bin/sh
-set -eu
+main() {
+    set -eu
+    : "${APT_SEARCH:=/usr/bin/apt}"
+    script_name=${0##*/}; script_name=${script_name%%.*}; script_name=${script_name%%_*}
+    argv0="/usr/bin/$script_name"
 
-script_name=${0##*/}; script_name=${script_name%%.*}; script_name=${script_name%%_*}
-argv0="/usr/bin/$script_name"
+    WITH_SUDO='install remove purge update upgrade full-upgrade dist-upgrade autoremove clean autoclean download hold unhold edit-sources'
 
-WITH_SUDO="install remove purge update upgrade full-upgrade dist-upgrade autoremove clean autoclean download hold unhold edit-sources"
-CMDS="/usr/bin/aptitude /usr/bin/apt-cache"
+    needs_sudo=0
+    search_cmd=""
+    use_search_cmd=0
 
-needs_sudo=0
-search_cmd=""
-use_search_cmd=0
+    if [ $# -gt 0 ]; then
+        for arg in "$@"; do
+            if [ "$arg" = "search" ]; then
+                use_search_cmd=1
+                [ ! -x "$APT_SEARCH" ] && APT_SEARCH="$argv0"
+                break
+            fi
 
-if [ $# -gt 0 ]; then
-    for arg in "$@"; do
-        if [ "$arg" = "search" ]; then
-            use_search_cmd=1
-            for cmd in $CMDS "$argv0"; do
-                if [ -x "$cmd" ]; then
-                    search_cmd="$cmd"
-                    break
-                fi
-            done
-            break
-        fi
+            if [ "$(id -u)" -gt 0 ]; then
+                case " $WITH_SUDO " in
+                    *" $arg "*) needs_sudo=1; break ;;
+                esac
+            fi
 
-        if [ "$(id -u)" -gt 0 ]; then
-            case " $WITH_SUDO " in
-                *" $arg "*) needs_sudo=1; break ;;
+            case "$arg" in
+                -*) ;;  # continuar
+                *) break ;;
             esac
-        fi
+        done
+    else
+        set -- --help
+    fi
 
-        case "$arg" in
-            -*) ;;  # continuar
-            *) break ;;
-        esac
-    done
-else
-    set -- --help
-fi
+    if [ "$use_search_cmd" -eq 1 ]; then
+        exec "$APT_SEARCH" "$@"
+    elif [ "$needs_sudo" -eq 1 ]; then
+        exec /usr/bin/sudo "$argv0" "$@"
+    else
+        exec "$argv0" "$@"
+    fi
+}
 
-if [ "$use_search_cmd" -eq 1 ]; then
-    exec "$search_cmd" "$@"
-elif [ "$needs_sudo" -eq 1 ]; then
-    exec /usr/bin/sudo "$argv0" "$@"
-else
-    exec "$argv0" "$@"
-fi
+main "$@"
